1da46f0be784db965e5f43f36c9d410bec4f4a36
Daria <d.ykhmmm@gmail.com>
2025-10-20 13:07:21 +0300

    handle null return in SendTcpRequest by throwing exception

--- NetSdrClientApp/NetSdrClient.cs
+++ NetSdrClientApp/NetSdrClient.cs
@@ -138,7 +138,7 @@ namespace NetSdrClientApp
             if (!_tcpClient.Connected)
             {
                 Console.WriteLine("No active connection.");
-                return null;
+                throw new InvalidOperationException("Not connected to a server.");
             }
 
             responseTaskSource = new TaskCompletionSource<byte[]>(TaskCreationOptions.RunContinuationsAsynchronously);
